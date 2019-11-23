c6092e72f964cca017a3fc3c2a3d71f2fd6b48a2
n18011 <n18011@std.it-college.ac.jp>
2019-11-24 03:04:09 +0900

    manifest changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 
 import {
   Container,
   Box
 } from '@material-ui/core'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { makeStyles } from '@material-ui/core/styles'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import purple from '@material-ui/core/colors/purple'
@@ -16,23 +15,20 @@ import Lyrics from './components/Lyrics'
 const useStyles = makeStyles(theme => ({
   container: {
     padding: 0,
-    backgroundColor: theme.palette.common.black
   },
 }))
 
-export default () => {
-  const classes = useStyles()
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: light)')
-
-  const theme = useMemo(() =>
-    createMuiTheme({
+  const theme = createMuiTheme({
       palette: {
         primary: purple,
         secondary: green,
         type: 'dark'
       }
     })
-  , [prefersDarkMode])
+
+
+export default () => {
+  const classes = useStyles()
 
   return (
     <>
