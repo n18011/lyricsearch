28c63b5b54b945aa80ad54bab851602b5fc1c8df
n18011 <n18011@std.it-college.ac.jp>
2019-11-24 18:45:47 +0900

    add 192px.png, and addHomeScreenIcon, appbar flex

--- src/components/Lyrics.jsx
+++ src/components/Lyrics.jsx
@@ -3,13 +3,13 @@ import React, {useContext} from 'react'
 import {
   AppBar,
   Toolbar,
-  TextField,
   IconButton,
-  Icon
+  InputBase
 } from '@material-ui/core'
-import {makeStyles} from '@material-ui/core/styles'
+import {fade, makeStyles} from '@material-ui/core/styles'
 import SearchIcon from '@material-ui/icons/Search';
 import QueueMusicRoundedIcon from '@material-ui/icons/QueueMusicRounded';
+import AddToHomeScreenIcon from '@material-ui/icons/AddToHomeScreen';
 
 import ScrollTop from './ScrollTop.jsx'
 import { LyricsContext } from '../context/LyricsProvider'
@@ -21,6 +21,46 @@ const useStyles = makeStyles(theme => ({
   menuButton: {
     marginRight: theme.spacing(2),
   },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
+  installButton:{
+    marginLeft: theme.spacing(1)
+  }
 }))
 
 export default ({children}) => {
@@ -29,7 +69,8 @@ export default ({children}) => {
   return (
     <>
     <header className={classes.root}>
-    <AppBar position="static">
+    <AppBar >
+            <Toolbar >
       <IconButton
             edge="start"
             color="inherit"
@@ -39,22 +80,33 @@ export default ({children}) => {
       <QueueMusicRoundedIcon/>
           </IconButton>
 
-            <Toolbar >
-            <TextField
-              id="filled-secondary"
-              variant="outlined"
-              color="secondary"
-              onChange={(e) => setValue(e.target.value)}
-              InputProps={{
-                startAdornment: (
-                  <IconButton type="submit" aria-label="search">
-                    <SearchIcon />
-                  </IconButton>
-                ),
+{!isShow ? (
+              <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
               }}
+              onChange={(e) => setValue(e.target.value)}
+              inputProps={{ 'aria-label': 'search' }}
             />
+          </div>
 
+): <div/>}
 
+      <IconButton
+            edge="end"
+            color="inherit"
+            className={classes.installButton}
+            aria-label="open drawer"
+          >
+            <AddToHomeScreenIcon/>
+          </IconButton>
+          
             </Toolbar>
           </AppBar>
           <Toolbar id='back-to-top-anchor' />
